0c5fa6251853771ef149a6b9465c78629737b572
Emre Kentli <emrekentlii@gmail.com>
2023-01-19 01:06:49 +0300

    first commit

---
+++ Zikirmatik/WindowsFormsApplication3/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.lblzikirsayisi = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.BackColor = System.Drawing.Color.Transparent;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioButton1.ForeColor = System.Drawing.Color.Black;
+            this.radioButton1.Location = new System.Drawing.Point(245, 26);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(122, 22);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.Text = " Subhanallah";
+            this.radioButton1.UseVisualStyleBackColor = false;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.BackColor = System.Drawing.Color.Transparent;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioButton2.ForeColor = System.Drawing.Color.Black;
+            this.radioButton2.Location = new System.Drawing.Point(11, 35);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(125, 22);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "Elhamdulillah";
+            this.radioButton2.UseVisualStyleBackColor = false;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.BackColor = System.Drawing.Color.Transparent;
+            this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioButton3.ForeColor = System.Drawing.Color.Black;
+            this.radioButton3.Location = new System.Drawing.Point(124, 3);
+            this.radioButton3.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(142, 26);
+            this.radioButton3.TabIndex = 1;
+            this.radioButton3.Text = "Allahu Ekber";
+            this.radioButton3.UseVisualStyleBackColor = false;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // lblzikirsayisi
+            // 
+            this.lblzikirsayisi.AutoSize = true;
+            this.lblzikirsayisi.BackColor = System.Drawing.Color.Transparent;
+            this.lblzikirsayisi.Font = new System.Drawing.Font("Monotype Corsiva", 54.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblzikirsayisi.ForeColor = System.Drawing.Color.Transparent;
+            this.lblzikirsayisi.Location = new System.Drawing.Point(74, 110);
+            this.lblzikirsayisi.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblzikirsayisi.Name = "lblzikirsayisi";
+            this.lblzikirsayisi.Size = new System.Drawing.Size(0, 87);
+            this.lblzikirsayisi.TabIndex = 2;
+            this.lblzikirsayisi.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Transparent;
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button4.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.button4.Location = new System.Drawing.Point(170, 246);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(78, 71);
+            this.button4.TabIndex = 5;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(254, 119);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 25);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "0";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.button3.Location = new System.Drawing.Point(259, 214);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(18, 24);
+            this.button3.TabIndex = 5;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(130, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(373, 357);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblzikirsayisi);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.Name = "Form1";
+            this.Text = "Zikirmatik";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Label lblzikirsayisi;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Zikirmatik/WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Media;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        Int32 zs;
+        Int32 zt;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+      
+
+        
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            button4.Text =" ";
+         zs=0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+          
+
+            private void label1_Click(object sender, EventArgs e)
+            {
+
+            }
+
+            private void button3_Click(object sender, EventArgs e)
+            {
+                label1.Text = " ";
+                zs = 0;
+                zt = 0;
+                label2.Text = " ";
+            }
+
+            private void button4_Click_1(object sender, EventArgs e)
+            {
+                zs++;
+                if (zs % 33 == 0)
+                {
+                    label1.ForeColor = Color.Red;
+                    zt++;
+                    label2.Text = zt.ToString();
+                }
+                label1.Text = zs.ToString();
+                if (zs % 33 == 1)
+                {
+                    label1.ForeColor = Color.Black;
+                    
+                }
+                
+            }
+
+            private void radioButton3_CheckedChanged(object sender, EventArgs e)
+            {
+                 
+            SoundPlayer ses = new SoundPlayer();
+            string dizin = Application.StartupPath + "\\song.wav";
+            ses.SoundLocation = dizin;
+            ses.Play();
+      
+            }
+
+            private void radioButton2_CheckedChanged(object sender, EventArgs e)
+            {
+          
+            SoundPlayer ses = new SoundPlayer();
+            string dizin = Application.StartupPath + "\\sonng.wav";
+            ses.SoundLocation = dizin;
+            ses.Play();
+        
+            }
+
+            private void radioButton1_CheckedChanged(object sender, EventArgs e)
+            {
+                SoundPlayer ses = new SoundPlayer();
+                string dizin = Application.StartupPath + "\\sonnng.wav";
+                ses.SoundLocation = dizin;
+                ses.Play();
+            }
+
+        }
+    }
+
